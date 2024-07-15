cc873d00073f9aa12485f42bd4ed9a4110072c31
PaladuguRenuka <166904796+PaladuguRenuka@users.noreply.github.com>
2024-07-15 11:28:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function stringChop(str, size) {
   // your code here
+	let chunkString = []
+	let resString = []
+	let count = 0;
+	for (let i = 0; i < str.length; i++) {
+		chunkString.push(str[i])
+		count++
+		if (count == size-1) {
+			count = 0
+			resString.push(chunkString)
+		}
+	}
+	resString.push(chunkString)
+	return resString
 }
 
 // Do not change the code below
